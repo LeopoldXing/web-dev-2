9658fd214b69b103e1341edeb46c467cac95d180
Rup <rup.singh.bhamra@gmail.com>
2024-06-03 11:42:58 -0500

    Config Array Updated! Added all image details in the same array.

--- module1/assignment/working_dir/Intro to PHP.php
+++ module1/assignment/working_dir/Intro to PHP.php
@@ -7,64 +7,52 @@
  * Description: Assignment 1 of Web Dev 2 course
  ****************/
 
-$config = [
-
-    'gallery_name' => 'Leopold\' s Gallery',
-
-    'unsplash_categories' => ['Travel', 'Animals', 'Nature', 'Business', 'Fashion', 'Sports'],
-
-    'local_images' => [
-        "philipp-kammerer-Travel.jpg",
-        "sanjoy-saha-Animals.jpg",
-        "v2osk-Nature.jpg",
-        "israel-andrade-Business.jpg",
-        "dami-adebayo-Fashion.jpg",
-        "markus-spiske-Sports.jpg"
-    ],
-
-    "imageInfoList" => [
-        "philipp-kammerer-Travel.jpg" => ["Eva Darron", "https://unsplash.com/@evadarron"],
-        "sanjoy-saha-Animals.jpg" => ["sanjoy saha", "https://unsplash.com/@rupam118"],
-        "v2osk-Nature.jpg" => ["v2osk", "https://unsplash.com/@v2osk"],
-        "israel-andrade-Business.jpg" => ["Israel Andrade", "https://unsplash.com/@israelandrxde"],
-        "dami-adebayo-Fashion.jpg" => ["Dami Adebayo", "https://unsplash.com/@dammypayne"],
-        "markus-spiske-Sports.jpg" => ["Markus Spiske", "https://unsplash.com/@markusspiske"]
-    ]
-
-];
-
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="main.css">
-  <title><?php echo $config["gallery_name"] ?></title>
-</head>
-<body>
-<!-- Remember that alternative syntax is good and html inside php is bad -->
-<h1><?php echo $config["gallery_name"] ?></h1>
-<div id="gallery">
-    <?php
-    foreach ($config['unsplash_categories'] as $index => $category) {
-        echo "<div><h2>$category</h2><img src=\"https://source.unsplash.com/300x200/?'.$category.'\" alt=\"nature image\"></div>";
-    }
-    ?>
-</div>
-<h1><?php echo count($config["local_images"]) ?> Large Images</h1>
-<div id="large-images">
-    <?php
-    $output = '';
-    foreach ($config["imageInfoList"] as $key => $imageInfo) {
-        $output .= "<img src='./images/$key' alt='" . $key . "'>";
-        $output .= '<h3 class="photographer"><a href="' . $imageInfo[1] . '">' . $imageInfo[0] . '</a></h3>';
-    }
-    echo $output;
-    ?>
-</div>
-</body>
-</html>
+ $config = [
+     'gallery_name' => 'Leopold\'s Gallery',
+ 
+     'unsplash_categories' => ['Travel', 'Animals', 'Nature', 'Business', 'Fashion', 'Sports'],
+ 
+     'local_images' => [
+         ['filename' => 'philipp-kammerer-Travel.jpg', 'photographer' => 'Eva Darron', 'unsplash_url' => 'https://unsplash.com/@evadarron'],
+         ['filename' => 'sanjoy-saha-Animals.jpg', 'photographer' => 'Sanjoy Saha', 'unsplash_url' => 'https://unsplash.com/@rupam118'],
+         ['filename' => 'v2osk-Nature.jpg', 'photographer' => 'v2osk', 'unsplash_url' => 'https://unsplash.com/@v2osk'],
+         ['filename' => 'israel-andrade-Business.jpg', 'photographer' => 'Israel Andrade', 'unsplash_url' => 'https://unsplash.com/@israelandrxde'],
+         ['filename' => 'dami-adebayo-Fashion.jpg', 'photographer' => 'Dami Adebayo', 'unsplash_url' => 'https://unsplash.com/@dammypayne'],
+         ['filename' => 'markus-spiske-Sports.jpg', 'photographer' => 'Markus Spiske', 'unsplash_url' => 'https://unsplash.com/@markusspiske']
+     ]
+ ];
+ 
+ ?>
+ 
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="main.css">
+   <title><?php echo $config["gallery_name"]; ?></title>
+ </head>
+ <body>
+ <h1><?php echo $config["gallery_name"]; ?></h1>
+ <div id="gallery">
+     <?php
+     foreach ($config['unsplash_categories'] as $category) {
+         echo "<div><h2>$category</h2><img src=\"https://source.unsplash.com/300x200/?$category\" alt=\"$category image\"></div>";
+     }
+     ?>
+ </div>
+ <h1><?php echo count($config["local_images"]); ?> Large Images</h1>
+ <div id="large-images">
+     <?php
+     foreach ($config["local_images"] as $image) {
+         echo "<div>";
+         echo "<img src='./images/{$image['filename']}' alt='{$image['filename']}'>";
+         echo "<h3 class='photographer'><a href='{$image['unsplash_url']}'>{$image['photographer']}</a></h3>";
+         echo "</div>";
+     }
+     ?>
+ </div>
+ </body>
+ </html>
+ 
